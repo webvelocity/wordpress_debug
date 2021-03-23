45da363c0351497325e79329139cd234bca56e26
Kevin Mpofu <40406337+webvelocity@users.noreply.github.com>
2021-03-23 15:59:44 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+// Enable WP_DEBUG mode
+define( 'WP_DEBUG', true );
+
+// Enable Debug logging to the /wp-content/debug.log file
+define( 'WP_DEBUG_LOG', true );
+
+// Disable display of errors and warnings
+define( 'WP_DEBUG_DISPLAY', false );
+@ini_set( 'display_errors', 0 );
+
+// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
+define( 'SCRIPT_DEBUG', true );
+
+
+
+
+/****
+WRITE TO DEUG.log PHP
+***/
+
+
+/****
+add to functions.php
+***/
+if (!function_exists('write_log')) {
+	function write_log ( $log )  {
+		if ( true === WP_DEBUG ) {
+			if ( is_array( $log ) || is_object( $log ) ) {
+				error_log( print_r( $log, true ) );
+			} else {
+				error_log( $log );
+			}
+		}
+	}
+}
+
+//Usage
+write_log($my_variable);
